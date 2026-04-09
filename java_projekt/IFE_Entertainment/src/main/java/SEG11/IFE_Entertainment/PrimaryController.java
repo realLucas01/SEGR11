79de939607659eb0fef2f8859e0dbff97eb0ecf1
Lucas <lucas.rumann@gmx.de>
2026-04-09 10:35:30 +0200

    Setup Project with Maven and Java FX
    Java Version is 21LTS
    JavaFX Version is 21.0.2
    Maven Version is 3.9, release is 12

---
+++ java_projekt/IFE_Entertainment/src/main/java/SEG11/IFE_Entertainment/PrimaryController.java
@@ -0,0 +1,12 @@
+package SEG11.IFE_Entertainment;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
---
+++ java_projekt/IFE_Entertainment/src/main/java/SEG11/IFE_Entertainment/SecondaryController.java
@@ -0,0 +1,12 @@
+package SEG11.IFE_Entertainment;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
