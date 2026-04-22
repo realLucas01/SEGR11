56e52955b374f7e12abe66d145397ad0c39ffab2
Domenik-htw <s89439@htw-dresden.de>
2026-04-22 17:03:37 +0200

    yee
    wie gesagt bindung an java fehlt

--- java_projekt/IFE_Entertainment/src/main/java/SEG11/IFE_Entertainment/UIController/MainMenuController.java
+++ java_projekt/IFE_Entertainment/src/main/java/SEG11/IFE_Entertainment/UIController/MainMenuController.java
@@ -1,26 +1,33 @@
 package SEG11.IFE_Entertainment.UIController;
 
 import java.io.IOException;
-
 import SEG11.IFE_Entertainment.App;
 import javafx.fxml.FXML;
 
 public class MainMenuController {
 
-	// TODO in startGame() umändern und implementieren
     @FXML
     private void switchToGame() throws IOException {
         App.setRoot("FourConnectGame");
-        // TODO
     }
-    
+
     @FXML
-    private void openSettings() throws IOException{
-    	// TODO
+    private void openModeMenu() throws IOException {
+        App.setRoot("ModeMenu");
     }
-    
+
     @FXML
-    private void backToMainMenu() throws IOException{
-    	// TODO
+    private void openSettings() throws IOException {
+        App.setRoot("Settings");
+    }
+
+    @FXML
+    private void openHelp() throws IOException {
+        App.setRoot("Help");
+    }
+
+    @FXML
+    private void onExit() {
+        System.exit(0);
     }
 }
