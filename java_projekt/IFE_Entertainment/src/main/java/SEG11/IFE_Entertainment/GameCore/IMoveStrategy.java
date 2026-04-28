1ad00cfe18e0c14511e9586835cfc851a037d1fb
tamaluc <96485045+tamaluc@users.noreply.github.com>
2026-04-28 21:26:22 +0200

    IMoveStrat JDoc

--- java_projekt/IFE_Entertainment/src/main/java/SEG11/IFE_Entertainment/GameCore/IMoveStrategy.java
+++ java_projekt/IFE_Entertainment/src/main/java/SEG11/IFE_Entertainment/GameCore/IMoveStrategy.java
@@ -1,5 +1,32 @@
+/*
+ * 1.0 2026-04-28 Luca Tauscher
+ *
+ * Copyright (c) 2025-2026 Gervithrall Systems GmbH. All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Gervithrall Systems GmbH
+ * ("Confidential Information"). You shall not disclose such Confidential Information and shall use
+ * it only in accordance with the terms of the license agreement you entered into with Gervithrall
+ * Systems GmbH.
+ *
+ * GERVITHRALL SYSTEMS GMBH MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE
+ * SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT. GERVITHRALL SYSTEMS GMBH
+ * SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR
+ * DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
+ */
+
 package SEG11.IFE_Entertainment.GameCore;
 
+/**
+ * Das Interface für das Spielfeld
+ *
+ */
 public interface IMoveStrategy {
+
+	/**
+	 * Wählt anhand des übergebenen Spielfeldes einen Zug aus und spielt diesen.
+	 *
+	 * @param board
+	 */
 	void chooseMove(IPlayArea board);
 }
