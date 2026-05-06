1dc947f3679baecd3a2fdd7c20ba7d5bf604dbd5
tamaluc <96485045+tamaluc@users.noreply.github.com>
2026-05-07 00:32:25 +0200

    removed unnessasary java doc

--- java_projekt/IFE_Entertainment/src/main/java/module-info.java
+++ java_projekt/IFE_Entertainment/src/main/java/module-info.java
@@ -1,27 +1,3 @@
-/*
- * 1.0 2026-04-22 Luca Tauscher
- *
- * Copyright (c) 2025-2026 Gervithrall Systems GmbH. All Rights Reserved.
- *
- * This software is the confidential and proprietary information of
- * Gervithrall Systems GmbH ("Confidential Information"). You shall not
- * disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license agreement you entered into
- * with Gervithrall Systems GmbH.
- *
- * GERVITHRALL SYSTEMS GMBH MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT
- * THE SUITABILITY OF THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING
- * BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT. GERVITHRALL SYSTEMS GMBH
- * SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT
- * OF USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
- */
-
-/**
- * JavaDoc Luca pls
- * 
- * @author Luca Tauscher
- */
 module SEG11.IFE_Entertainment {
     requires javafx.controls;
     requires javafx.fxml;
