ab083263cca37b409f4ac31f9824c7f7ffbfd349
Domenik-htw <s89439@htw-dresden.de>
2026-06-02 18:44:50 +0200

    a

--- java_projekt/ife.entertainment/src/test/java/segeleven/ife/entertainment/fourconnecttest/FourConnectGameBoardTest.java
+++ java_projekt/ife.entertainment/src/test/java/segeleven/ife/entertainment/fourconnecttest/FourConnectGameBoardTest.java
@@ -207,7 +207,9 @@ class FourConnectGameBoardTest {
 
       for (int col = 0; col < board.getColumns(); col++) {
         for (int row = 0; row < board.getRows(); row++) {
-          assertEquals(board.getCellOwner(col, row).getType(), copy.getCellOwner(col, row).getType());
+          assertEquals(
+              board.getCellOwner(col, row).getType(), copy.getCellOwner(col, row).getType()
+              );
         }
       }
     }
